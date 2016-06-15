08102714aad6816dda10b5cc817c2f6c0ab0341d
Huber <simon.huber@capgemini.com>
2016-06-15 10:32:20 +0200

    backup

--- samples/core/src/test/java/citrus/SampleIT.java
+++ samples/core/src/test/java/citrus/SampleIT.java
@@ -1,29 +1,24 @@
 package citrus;
 
-import org.junit.Test;
-
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.junit.AbstractJUnit4CitrusTest;
-
+import com.consol.citrus.dsl.JUnit4CitrusTestDesigner;
+import org.junit.Test;
 /**
- * TODO: Description
- *
- * @author Unknown
- */
-public class SampleIT extends AbstractJUnit4CitrusTest {
-  @Test
-  @CitrusTest
-  public void EchoSampleIT() {
-
-    variable("time", "citrus:currentDate()");
-    echo("Hello Citrus!");
-    echo("CurrentTime is: ${time}");
-  }
-
-  @Test
-  @CitrusTest(name = "EchoIT")
-  public void echoTest() {
-
-    echo("Hello Citrus!");
-  }
+* TODO: Description
+*
+* @author Unknown
+*/
+public class SampleIT extends JUnit4CitrusTestDesigner {
+@Test
+@CitrusTest
+public void EchoSampleIT() {
+variable("time", "citrus:currentDate()");
+echo("Hello Citrus!");
+echo("CurrentTime is: ${time}");
+}
+@Test
+@CitrusTest(name = "EchoIT")
+public void echoTest() {
+echo("Hello Citrus!");
+}
 }
