0235bc58cd52ab737f3960e76240bfbe367e58bb
Huber <simon.huber@capgemini.com>
2016-06-21 08:55:07 +0200

    backup

--- samples/core/src/test/java/io/oasp/gastronomy/restaurant/citrus_junit/HttpAction.java
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/citrus_junit/HttpAction.java
@@ -1,10 +1,12 @@
 package io.oasp.gastronomy.restaurant.citrus_junit;
 
 import org.junit.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.dsl.junit.JUnit4CitrusTestDesigner;
+import com.consol.citrus.message.MessageType;
 
 /**
  * TODO shuber This type ...
@@ -12,17 +14,31 @@ import com.consol.citrus.dsl.junit.JUnit4CitrusTestDesigner;
  * @author shuber
  * @since dev
  */
+
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @SpringApplicationConfiguration(classes = { SpringBootApp.class })
+// @WebIntegrationTest("server.port:8090")
+// @ActiveProfiles(profiles = { OaspProfile.JUNIT_TEST })
+// @Transactional
 public class HttpAction extends JUnit4CitrusTestDesigner {
   @Test
   @CitrusTest
   public void httpActionTest() {
 
-    http().client("httpClient").post("/customer")
-        .payload("<customer>" + "<id>citrus:randomNumber()</id>" + "<name>testuser</name>" + "</customer>")
-        .header("CustomHeaderId", "${custom_header_id}").contentType("text/xml").accept("text/xml, */*");
+    // variable("custom_header_id", 3);
+    // http().client("salesmanagementClient").get("table/101").payload("").header("").contentType("application/json")
+    // .accept("application/json, */*");
 
-    http().client("httpClient").response(HttpStatus.OK)
-        .payload("<customerResponse>" + "<success>true</success>" + "</customerResponse>")
-        .header("CustomHeaderId", "${custom_header_id}").version("HTTP/1.1");
+    // http().client("salesmanagementClient").get("table/101").contentType("application/json")
+    // .accept("application/json, */*");
+
+    // http().client("salesmanagementClientPost").post("order/search").payload("{state: \"OPEN\", tableId: \"101\"}")
+    // .header("").contentType("application/json").accept("application/json, text/plain, */*");
+    // http().client("salesmanagementClientPost").response(HttpStatus.OK).payload("").header("").version("HTTP/1.1");
+    //
+    send("salesmanagementClientPost").payload("{state: \"OPEN\", tableId: \"101\"}").http().method(HttpMethod.POST)
+        .path("order/search");
+    receive("salesmanagementClientPost").messageType(MessageType.JSON).http().status(HttpStatus.OK);
+
   }
 }
