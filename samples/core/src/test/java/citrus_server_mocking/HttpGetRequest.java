7a5e6c81e615a1686ac61502ee9b4a528e0459c3
Huber <simon.huber@capgemini.com>
2016-07-05 08:46:38 +0200

    http client changed, works

--- samples/core/src/test/java/citrus_server_mocking/HttpGetRequest.java
+++ samples/core/src/test/java/citrus_server_mocking/HttpGetRequest.java
@@ -6,17 +6,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.junit.Test;
-import org.springframework.core.io.ClassPathResource;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-import com.consol.citrus.annotations.CitrusResource;
-import com.consol.citrus.dsl.design.TestDesigner;
-
+import io.oasp.gastronomy.restaurant.SpringBootApp;
 import io.oasp.gastronomy.restaurant.general.common.base.AbstractRestServiceTest;
+import io.oasp.gastronomy.restaurant.salesmanagement.service.impl.rest.SalesmanagementRestTestConfiguration;
 
 /**
  * TODO shuber This type ...
@@ -24,7 +26,9 @@ import io.oasp.gastronomy.restaurant.general.common.base.AbstractRestServiceTest
  * @author shuber
  * @since dev
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = { SpringBootApp.class, SalesmanagementRestTestConfiguration.class })
+@TestPropertySource(properties = { "flyway.locations=filesystem:src/test/resources/db/tablemanagement" })
 public class HttpGetRequest extends AbstractRestServiceTest {
 
   /**
@@ -33,13 +37,14 @@ public class HttpGetRequest extends AbstractRestServiceTest {
    */
 
   @Test
-  public void httpGetRequestActionTest(@CitrusResource TestDesigner designer) {
+  public void httpGetRequestActionTest() {
 
     RestTemplate template = new RestTemplate();
 
     HttpHeaders headers = new HttpHeaders();
-    headers.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+    headers.add("Accept", "text/plain,application/json,application/*+json,*/*");
     headers.add("Connection", "keep-alive");
+    headers.add("Content-Type", "text/plain;charset=ISO-8859-1");
     HttpEntity<String> getRequest = new HttpEntity<>(headers);
     ResponseEntity<String> getResponse =
         template.exchange("http://localhost:8081/test", HttpMethod.GET, getRequest, String.class);
@@ -49,29 +54,10 @@ public class HttpGetRequest extends AbstractRestServiceTest {
     try {
       encodedFileContent = Files.readAllBytes(Paths.get("src/test/resources/orderPositionPayload.json"));
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     String fileContent = new String(encodedFileContent, Charset.defaultCharset());
-
-    assertThat(getResponse).isEqualTo(new ClassPathResource("orderPositionPayload.json"));
-    assertThat(getResponse).isEqualTo(fileContent);
-    System.out.println(getResponse.getBody());
-
-    // RestTemplate template = new RestTemplate();
-    //
-    // HttpHeaders headers = new HttpHeaders();
-    // // headers.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-    // headers.add("Connection", "keep-alive");
-    // HttpEntity<String> getRequest = new HttpEntity<>(headers);
-    // ResponseEntity<String> getResponse =
-    // template.exchange("http://localhost:8081/test", HttpMethod.GET, getRequest, String.class);
-    // assertThat(getResponse.getBody()).isNotNull();
-    // // URL url = getClass().getResource("orderPositionPayload.json");
-    //
-    // assertThat(getResponse.getBody().toString())
-    // .isEqualTo(FileUtils.readLines(new File("src/test/resources/orderPositionPayload.json")));
-    // System.out.println(getResponse.getBody());
+    assertThat(getResponse.getBody()).isEqualTo(fileContent);
 
   }
 }
