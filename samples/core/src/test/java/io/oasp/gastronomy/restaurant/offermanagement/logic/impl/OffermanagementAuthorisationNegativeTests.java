6eb30296c9d71b5e7fef478f3e2176d9b1a14a6d
Huber <simon.huber@capgemini.com>
2016-08-31 15:33:22 +0200

    clean solution for login without permissions

--- samples/core/src/test/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementAuthorisationNegativeTests.java
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementAuthorisationNegativeTests.java
@@ -14,7 +14,6 @@ import io.oasp.gastronomy.restaurant.SpringBootApp;
 import io.oasp.gastronomy.restaurant.common.builders.MealEtoBuilder;
 import io.oasp.gastronomy.restaurant.common.test.SampleCreator;
 import io.oasp.gastronomy.restaurant.general.common.TestUtil;
-import io.oasp.gastronomy.restaurant.general.common.api.constants.PermissionConstants;
 import io.oasp.gastronomy.restaurant.general.common.base.AbstractRestServiceTest;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.Offermanagement;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.MealEto;
@@ -40,8 +39,7 @@ public class OffermanagementAuthorisationNegativeTests extends AbstractRestServi
   public void setup() {
 
     getDbTestHelper().resetDatabase();
-    // TODO fragen ob es nicht sinnvoll wäre auch einen Login für Testzwecke ohne Berechtigung zu speichern
-    TestUtil.login("chief", PermissionConstants.DELETE_ORDER);
+    TestUtil.login("chief");
   }
 
   @After
